bde2fe867ee727f96b322adba3abcb4e240aaf89
Liana Tomecek <liana.tomecek@ucalgary.ca>
2023-05-27 19:58:32 -0600

    successfully drawing a grid but nothing else
    asdasdasda

--- Assets/_Scripts/MapGrid.cs
+++ Assets/_Scripts/MapGrid.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,61 +12,78 @@ public class MapGrid : MonoBehaviour
 
     private MeshFilter planeMesh;
     private Vector3[] corners;
-    private MapTile[,] tiles;
+    private List<Vector3> lineVerts = new List<Vector3>();
 
-    //private LineRenderer lr;
+    private MapTile[,] tiles;
+    private LineRenderer lr;
 
 
     // Start is called before the first frame update
     void Start()
     {
         planeMesh = GetComponent<MeshFilter>();
-        corners = new Vector3[5];
+        corners = new Vector3[4];
 
         //init corners array
         corners[2] = transform.TransformPoint(planeMesh.sharedMesh.vertices[0]);
         corners[3] = transform.TransformPoint(planeMesh.sharedMesh.vertices[10]);
         corners[1] = transform.TransformPoint(planeMesh.sharedMesh.vertices[110]);
         corners[0] = transform.TransformPoint(planeMesh.sharedMesh.vertices[120]);
-        corners[4] = corners[0];
 
 
         //determine tile width and height based on params
-        tileWidth = Mathf.Abs((corners[1].x - corners[0].x)) / rows;
-        tileHeight = Mathf.Abs((corners[2].y - corners[0].y)) / columns;
+        tileWidth = Mathf.Abs((corners[1].x - corners[0].x)) / columns;
+        tileHeight = Mathf.Abs((corners[2].y - corners[0].y)) / rows;
 
 
-        //init tiles array 
-        MapTile temp;
+        //init tiles array
+        tiles = new MapTile[rows, columns];
+        MapTile newTile;
         Vector3 vert = corners[0];  //BEWARE: Not sure if this just copies the values or the actual reference
-
-        //ASSUMPTION: starting top-right then going across each row
+        
         for (int i = 0; i < rows; i++)
         {
+            lineVerts.Add(vert);
+
             for (int j = 0; j < columns; j++)
             {
-                temp = new MapTile(vert, tileWidth, tileHeight);
-                temp.setX(i);
-                temp.setY(j);
-
-                tiles[i, j] = temp;
-
+                newTile = new MapTile(vert, tileWidth, tileHeight, i, j);
                 vert.x += tileWidth;
-
             }
-            
+
+            lineVerts.Add(vert);
             vert.x = corners[0].x;
+            lineVerts.Add(vert);
             vert.y -= tileHeight;
         }
 
-        //lr = GetComponent<LineRenderer>();
+        //finish adding the vertices for the line renderer
+        Vector3 vert2;
+        vert = corners[3];
+        lineVerts.Add(vert);
 
-        //lr.startWidth = 0.01f;
-        //lr.endWidth = 0.01f;
+        for (int i = 0; i < columns; i++)
+        {
+            vert.x += tileWidth;
+            lineVerts.Add(vert);
+            vert2 = vert;
+            vert2.y = corners[0].y;
+            lineVerts.Add(vert2);
+            lineVerts.Add(vert);
+            
+        }
 
-        //lr.positionCount = 5;
-        //lr.SetPositions(corners);
-       
+
+
+        //draw the grid
+        lr = GetComponent<LineRenderer>();
+        lr.startWidth = 0.01f;
+        lr.endWidth = 0.01f;
+
+        lr.positionCount = lineVerts.Count;
+        lr.SetPositions(lineVerts.ToArray());
+
+
     }
 
     // Update is called once per frame
--- Assets/_Scripts/MapTile.cs
+++ Assets/_Scripts/MapTile.cs
@@ -9,57 +9,40 @@ public class MapTile
     private int x;
     private int y;      //grid coordinates? may not need them at all.
 
-
-    private LineRenderer lr;
-
     public Vector3[] cornerVertices;
 
     //private bool isOccupied;
     private int contentCode;
     private GameObject actor;
-    
 
-    public MapTile(Vector3 vert, float tileWidth, float tileHeight)
+
+    public MapTile(Vector3 vert, float tileWidth, float tileHeight, int i, int j)
     {
-        cornerVertices = new Vector3[5];
+        cornerVertices = new Vector3[4];
         cornerVertices[0] = vert;
         cornerVertices[1] = new Vector3(vert.x + tileWidth, vert.y, 0);
         cornerVertices[2] = new Vector3(vert.x + tileWidth, vert.y - tileHeight, 0);
         cornerVertices[3] = new Vector3(vert.x, vert.y - tileHeight, 0);
-        cornerVertices[4] = cornerVertices[0];
-    }
-    
-    
-    void Start()
-    {
-        //cornerVertices = new Vector3[4];
-        lr = new LineRenderer();
-        //lr = GetComponent<LineRenderer>();
-        
-        drawBorder();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        y = i;
+        x = j;
     }
 
     //--------------------
     //  Public Functions  
     //--------------------
-    public void drawBorder()
-    {
-        lr.startWidth = 0.01f;
-        lr.endWidth = 0.01f;
+    //public void drawBorder()
+    //{
+    //    lr.loop = true;
+    //    lr.startWidth = 0.01f;
+    //    lr.endWidth = 0.01f;
 
-        lr.positionCount = 5;
-        lr.SetPositions(cornerVertices);
+    //    lr.positionCount = 4;
+    //    lr.SetPositions(cornerVertices);
 
 
-        Debug.Log("bitchasshoe");
+    //    //Debug.Log("bitchasshoe");
    
-    }
+    //}
 
 
     ////////////////
